b3ddcfcae8d86f27dc6a972db9f2126e826890a2
NazmulHasan18 <nazmul182218@gmail.com>
2023-05-02 15:33:53 +0600

    chef id data added

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3000;
+const chefs = require("./data/chefs.json");
+const foods = require("./data/foods.json");
 
 app.use(cors());
 
 app.get("/", (req, res) => {
    res.send("Hello World!");
+});
+
+app.get("/chefs", (req, res) => {
+   res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+   const id = req.params.id;
+   const chef = chefs.find((per) => per.id == id);
+
+   res.send(chef);
+});
+
+app.get("/foods", (req, res) => {
+   res.send(foods);
 });
 
 app.listen(port, () => {
